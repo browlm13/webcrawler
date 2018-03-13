fdbb53a4d1d18779251ab39830c7fb80477a7f50
LJ Brown <browlm13@gmail.com>
2018-03-13 00:05:36 -0400

    Update webcrawler.py

--- webcrawler.py
+++ webcrawler.py
@@ -7,6 +7,7 @@ from urllib.parse import urljoin
 import os
 import logging
 import json
+import glob
 
 #my lib
 from data_structures import data_structures as ds
@@ -65,6 +66,28 @@ def is_suburl(url, parent_url, url_alternates_map={}):
 	if common_path == parent_url:
 		return True
 	return False
+
+#
+#	output file name mthods
+#
+def document_directory_path(output_directory, url_id):
+	# document directory
+	directory_name = "doc_%s" % str(url_id)
+	directory_path = os.path.join(output_directory, directory_name)
+	return directory_path
+
+# URL meta data file name
+def url_meta_data_file_path(output_directory, url_id):
+	directory_path = document_directory_path(output_directory, url_id)
+	filename = "url_meta_data_%s.json" % str(url_id)
+	return os.path.join(directory_path, filename)
+	
+# Document ('tokens')file name
+def document_file_path(output_directory, url_id):
+	directory_path = document_directory_path(output_directory, url_id)
+	filename = "tokens_%s.json" % str(url_id)
+	return os.path.join(directory_path, filename)
+
 
 # give stopwords
 class Crawler():
@@ -149,13 +172,13 @@ class Crawler():
 		#self.url_alternates_map = create_url_alternates_map([self.seed_url], self.url_alternates_map)
 
 		# map url
-		seed_url = self.url_alternates_map[self.seed_url]
+		#seed_url = self.url_alternates_map[self.seed_url]
 
 		# find forbidden urls
 		self.forbidden_urls = []
 
 		# get the robots.txt url for the seed url
-		robots_url = urljoin(seed_url, "robots.txt")
+		robots_url = self.seed_url + "robots.txt"
 
 		# extract the forbidden routes
 		page_data = access_page.get_page_data(robots_url)
@@ -164,8 +187,15 @@ class Crawler():
 				if line.startswith('Disallow'): 
 					forbidden = line.split(':')[1].strip()
 
+					if forbidden[0] == '/':
+						forbidden = forbidden[1:]
+
+					seed_url = self.seed_url
+					if self.seed_url[-1] == '/':
+						seed_url = self.seed_url[:-1]
+
 					# add site prefix
-					self.forbidden_urls.append(urljoin(seed_url,forbidden))
+					self.forbidden_urls.append(seed_url + '/' + forbidden)
 	
 	def keep_indexing(self):
 
@@ -191,7 +221,7 @@ class Crawler():
 		logger.info("Saving URL Meta Data...")
 
 		# document directory
-		directory_path = self.document_directory_path(url_meta_data['url_id'])
+		directory_path = document_directory_path(self.output_directory, url_meta_data['url_id']) #self.document_directory_path(url_meta_data['url_id'])
 
 		# if output directory does not exist, create it
 		if not os.path.exists(directory_path): 
@@ -201,11 +231,11 @@ class Crawler():
 		# save url meta data file
 		#
 
-		filename = "url_meta_data_%d.json" % url_meta_data['url_id']
-		filepath = os.path.join(directory_path, filename)
+		filepath = url_meta_data_file_path(self.output_directory, url_meta_data['url_id'])
 
 		with open(filepath, 'w') as file:
 			file.write(json.dumps(url_meta_data))
+
 
 		logger.info("Saved URL Meta Data.")
 
@@ -216,7 +246,7 @@ class Crawler():
 		logger.info("Saving Document ('Tokens')...")
 
 		# document directory
-		directory_path = self.document_directory_path(url_id)
+		directory_path = document_directory_path(self.output_directory, url_id) #self.document_directory_path(url_id)
 
 		# if output directory does not exist, create it
 		if not os.path.exists(directory_path): 
@@ -226,8 +256,7 @@ class Crawler():
 		# save document ('tokens') file
 		#
 
-		filename = "tokens_%d.json" % url_id
-		filepath = os.path.join(directory_path, filename)
+		filepath = document_file_path(self.output_directory, url_id)
 
 		with open(filepath, 'w') as file:
 			file.write(json.dumps(tokens))
@@ -264,7 +293,7 @@ class Crawler():
 			#
 
 			# log info
-			logger.info("\nCrawling Site Number: %d" % len(self.site_graph))
+			logger.info("\nCrawling URL Number: %d" % len(self.site_graph))
 
 			# remove url from queue and get page data
 			target_url = self.queue.remove()
@@ -292,11 +321,13 @@ class Crawler():
 			url_id = len(self.site_graph)
 			self.url_id_map[target_url] = url_id
 
-			### 	URL meta data (page_data w/out 'plain_text', add url_id)
+			### 	URL meta data (page_data w/out 'plain_text', add url_id, add seed_url, mapped_url)
 
 			# create url meta data
 			url_meta_data = {k:v for k,v in page_data.items() if k != 'plain_text' }
 			url_meta_data['url_id'] = url_id
+			url_meta_data['seed_url'] = self.seed_url
+			url_meta_data['mapped_url'] = target_url
 
 			# save URL meta data
 			self.save_url_meta_data(url_meta_data)
@@ -314,11 +345,9 @@ class Crawler():
 			# save Document ('tokens')
 			self.save_document(tokens, url_id)
 
-
 			#
 			#	Add New Urls To Queue And Continue Crawling
 			#
-
 
 			# filter new found urls and add to queue
 			new_urls = self.filter_urls(page_data['absolute_out_links'])
@@ -328,16 +357,128 @@ class Crawler():
 					logger.info("\t\t- %s" % u)
 					self.queue.add(u)
 
-
 		# log info
 		logger.info("Finished Site Crawl: %s" % self.seed_url)
+
+
+		# save url id dictonary!?
+
+
+def build_site_graph(crawler_output_directory):
+
+	# url meta data list
+	site_graph = []
+
+	glob_phrase = document_directory_path(crawler_output_directory,'*')
+
+	for path in glob.glob(glob_phrase):
+		directory = os.path.split(path)[1]
+		url_id = int(directory.split('_')[-1])
+		
+		url_meta_data = {}
+		umd_file_path = url_meta_data_file_path(crawler_output_directory, url_id)
+
+		with open(umd_file_path) as json_data:
+			umd = json.load(json_data)
+		site_graph.append(umd)
+
+	return site_graph
+
+def build_url_id_map(crawler_output_directory):
+	site_graph = build_site_graph(crawler_output_directory)
+
+	url_id_map = ds.DoubleDict()
+	for umd in site_graph:
+		url = umd['mapped_url']
+		doc_id = umd['url_id']
+		url_id_map[doc_id] = url
+	return url_id_map
 
 
 if __name__ == '__main__':
 
 	SEED_URL = "http://lyle.smu.edu/~fmoore/"
-	MAX_URLS_TO_INDEX = 3
+	MAX_URLS_TO_INDEX = None
 	STOPWORDS_FILE = "stopwords.txt"
-	OUTPUT_DIRECTORY = "output"
-	c = Crawler()
-	c.crawl_site(SEED_URL, OUTPUT_DIRECTORY, MAX_URLS_TO_INDEX, STOPWORDS_FILE)
+	OUTPUT_DIRECTORY = "output2"
+
+	#
+	# 	Crawl
+	#
+
+	#c = Crawler()
+	#c.crawl_site(SEED_URL, OUTPUT_DIRECTORY, MAX_URLS_TO_INDEX, STOPWORDS_FILE)
+
+	#
+	#	display Url Meta Data
+	#
+
+
+	site_graph = build_site_graph(OUTPUT_DIRECTORY)
+	url_id_map = build_url_id_map(OUTPUT_DIRECTORY)
+	
+
+	for umd in site_graph:
+		doc_id = umd['url_id']
+		url = umd['mapped_url']
+		seed_url = umd['seed_url']
+		non_image_links = umd['absolute_out_links']
+		image_links = umd['absolute_image_links']
+		document_hash = umd['document_hash']
+
+		# duplicates
+		url_ids_with_duplicate_content = []
+		for umd2 in site_graph:
+			if (umd2['document_hash'] == document_hash) and (umd2['url_id'] is not doc_id):
+				url_ids_with_duplicate_content.append(umd2['url_id'])
+
+		print(doc_id)
+		print(url)
+		print("Broken: %s" % umd['broken'])
+
+		print("duplicates:")
+		print(url_ids_with_duplicate_content)
+
+	
+		print("image links:")
+		print(image_links)
+
+		internal_links = []
+		outbound_links = []
+		for l in non_image_links:
+			if is_suburl(l, seed_url):
+				internal_links.append(l)
+			else: 
+				outbound_links.append(l)
+		print("internal links:")
+		print(internal_links)
+		print("outbound_links:")
+		print(outbound_links)
+		print("\n\n")
+
+
+
+		
+
+	# for each page
+	#	report title
+	# 	report all 'inbounds' links
+	#	report all outgoing (out of bounds)
+
+	# report all broken
+	# read all type : "content_type": ... (gif, jpg, jpeg, png)
+	# report all duplicate documents
+	# create term document frequency matrix
+	# list 20 most common words
+
+"""
+
+	#
+	#	term frequency matrix
+	#
+
+	input:
+		-Seed websites (add to queue)
+		-N number of pages to index
+
+"""
